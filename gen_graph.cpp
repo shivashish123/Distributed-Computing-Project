4665e02ae7caa26664496cec84a8253fa1069e9d
atharva-sarage <atharva.sarage@gmail.com>
2021-04-18 20:50:52 +0530

    testing done and tester added

---
+++ gen_graph.cpp
@@ -0,0 +1,32 @@
+
+
+    // generating a tree in a simple way
+#include <bits/stdc++.h>
+using namespace std;
+
+int rand(int a, int b) {
+    return a + rand() % (b - a + 1);
+}
+
+int main(int argc, char* argv[]) {
+    srand(atoi(argv[1]));
+    int n = rand(2, 4);
+    int m = rand(n-1,n*(n-1)/2);
+    printf("%d %d 1\n", n , m);
+    set<pair<int,int>> edges;
+    for(int i = 2; i <= n; ++i) {
+        int u = rand(1,i-1);
+        int v = i;
+        printf("%d %d\n", u,v);
+        edges.insert({u,v});
+    }
+    while(edges.size()<m){
+        
+        int x = rand(1,n);
+        int y = rand(1,n);
+        if(x==y) continue;
+        if((edges.find({x,y})!=edges.end()) || (edges.find({y,x})!=edges.end())) continue;
+        edges.insert({x,y});
+        cout<<x<<" "<<y<<endl;
+    }
+}
